5ee0596b679295e4bc4d1eb5e654fcbe801dfb26
Ashish <majhiashish717@gamil.com>
2026-05-13 13:22:20 +0530

    Add Python calculator sample code

---
+++ sample_calculator.py
@@ -0,0 +1,19 @@
+print("        SIMPLE CALCULATOR     ")
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+print(" 1 for ADDITION \n 2 for SUBTRACTION \n 3 for PRODUCT \n 4 for DIVISION")
+n=int(input("Enter the for calculation : "))
+match n:
+    case 1:
+        print("Sum of the number",a,"and",b,"= ",a+b)
+    case 2:
+        print("Subtractilon of the number",a,"and",b,"=",a-b)
+    case 3:
+        print("Product of the number",a,"and",b,"= ",a*b)
+    case 4:
+        if b==0:
+            print("Not Possible")
+        else:
+            print("Division of the number",a,"and",b,"= ",a/b)
+    case _:
+        print("INVALID CHOICE")                            
